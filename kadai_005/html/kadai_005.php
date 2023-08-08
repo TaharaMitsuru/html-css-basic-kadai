2c5da647756526962467b2e0d9b652746a1a24cb
TaharaMitsuru <92862626+TaharaMitsuru@users.noreply.github.com>
2023-08-08 13:24:17 +0900

    Add files via upload

---
+++ kadai_005/html/kadai_005.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <title>5章課題</title>
+        <meta charset="UTF-8">
+        <meta name="description" content="このページは5章の課題です。">
+    </head>
+    <header>
+        <h1>私の趣味</h1>
+        <br>
+        <p>
+            私は中学時代にサッカーを始めました。
+            <br>
+            現在はサッカーのコーチとして活動をして、趣味のサッカーを楽しんでいます。
+        </p>
+        <img src="../images/ball.png">
+        <br>
+        <a href="https://twitter.com/samuraijuku">私のプロフィールはこちら</a>
+    </header>
+    <body>
+        <form>
+
+
+        </form>
+    </body>
+</html>
